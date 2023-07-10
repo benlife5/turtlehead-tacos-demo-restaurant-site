ec6f1533e5de5cb68bdb5cb725b145b74a9d0634
Ben Life <blife@yext.com>
2023-07-10 14:28:33 -0400

    updated ci

--- src/functions/onUrlChange/onChange1.ts
+++ src/functions/onUrlChange/onChange1.ts
@@ -2,5 +2,5 @@ import {OnUrlChangeArgument, OnUrlChangeResponse } from "@yext/pages";
 
 export default function onChange(request : OnUrlChangeArgument) : OnUrlChangeResponse {
   const { site } = request
-  console.log("OnUrlChange 2 Running")
+  console.log("OnUrlChange 1 Running")
 }
